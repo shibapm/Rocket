71b3c02953c0ea9dddbd4c37a399e5c3e1c502da
Franco Meloni <franco.meloni91@gmail.com>
2018-12-03 10:26:52 +0000

    Unify checks on HideDevDependenciesExecutorTests

--- Tests/RocketTests/HideDevDependenciesExecutorTests.swift
+++ Tests/RocketTests/HideDevDependenciesExecutorTests.swift
@@ -6,21 +6,20 @@ import TestSpy
 
 final class HideDevDependenciesExecutorTests: XCTestCase {
     func testItSendsTheHideDependenciesCallToTheDevDependenciesModifier() {
-        let devDependenciesModifier = SpyDevDependenciesModifier()
-        let executor = HideDevDependenciesExecutor(dictionary: ["package_path": "testPackage.swift"])
-        executor.devDependenciesModifier = devDependenciesModifier
-        executor.executeStep(version: "1.0.0", logger: Logger.testLogger)
-        
-        expect(devDependenciesModifier).to(haveReceived(.hideDependencies(packagePath: "testPackage.swift")))
+        checkDevDependencyModifierReceived(dictionary: ["package_path": "testPackage.swift"], expectedPackagePath: "testPackage.swift")
     }
     
     func testItUsesTheDefaultPackagePath() {
+        checkDevDependencyModifierReceived(dictionary: nil, expectedPackagePath: "Package.swift")
+    }
+    
+    func checkDevDependencyModifierReceived(dictionary: [String:Any]?, expectedPackagePath: String) {
         let devDependenciesModifier = SpyDevDependenciesModifier()
-        let executor = HideDevDependenciesExecutor(dictionary: nil)
+        let executor = HideDevDependenciesExecutor(dictionary: dictionary)
         executor.devDependenciesModifier = devDependenciesModifier
         executor.executeStep(version: "1.0.0", logger: Logger.testLogger)
         
-        expect(devDependenciesModifier).to(haveReceived(.hideDependencies(packagePath: "Package.swift")))
+        expect(devDependenciesModifier).to(haveReceived(.hideDependencies(packagePath: expectedPackagePath)))
     }
 }
 
