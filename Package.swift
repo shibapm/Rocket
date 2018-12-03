3a2805ef3a7261d798f0c867d3286ffd9fd8158a
Franco Meloni <franco.meloni91@gmail.com>
2018-12-03 11:04:57 +0000

    Add Komondor to the project

--- Package.swift
+++ Package.swift
@@ -18,7 +18,10 @@ let package = Package(
         .package(url: "https://github.com/Quick/Nimble", from: "7.3.1"),
         .package(url: "https://github.com/JohnSundell/ShellOut.git", from: "2.1.0"),
         .package(url: "https://github.com/f-meloni/TestSpy", from: "0.3.1"),
-        .package(url: "https://github.com/orta/PackageConfig.git", from: "0.0.1")
+        .package(url: "https://github.com/orta/PackageConfig.git", from: "0.0.1"),
+        // Dev Dependencies
+        .package(url: "https://github.com/orta/Komondor.git", from: "1.0.0"), // dev
+        .package(url: "https://github.com/nicklockwood/SwiftFormat.git", from: "0.35.8"), // dev
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -34,3 +37,16 @@ let package = Package(
             dependencies: ["RocketLib", "Nimble", "TestSpy"]),
     ]
 )
+
+#if canImport(PackageConfig)
+import PackageConfig
+
+let config = PackageConfig([
+        "komondor": [
+            "pre-commit": [
+                "swift run swiftFormat .",
+                "git add ."
+            ]
+        ]
+    ])
+#endif
