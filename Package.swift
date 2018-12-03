e3c398f9efc956459636fe67136c2047edff4ecd
Franco Meloni <franco.meloni91@gmail.com>
2018-12-03 19:05:39 +0000

    Updated rocket executable name

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .executable(
-            name: "Rocket",
+            name: "rocket",
             targets: ["Rocket"]
         ),
     ],
