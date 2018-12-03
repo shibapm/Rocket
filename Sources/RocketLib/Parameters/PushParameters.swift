67b40e9ef5262e39af82a46b2832c1114abc4646
Franco Meloni <franco.meloni91@gmail.com>
2018-12-03 13:30:30 +0000

    Support no-verify parameter on git push

--- Sources/RocketLib/Parameters/PushParameters.swift
+++ Sources/RocketLib/Parameters/PushParameters.swift
@@ -2,14 +2,17 @@
 struct PushParameters: StepParameters {
     let remote: String
     let branch: String
+    let noVerify: Bool
 
     enum CodingKeys: String {
         case remote
         case branch
+        case noVerify = "no_verify"
     }
 
     init(dictionary: [String: Any]?) {
         remote = dictionary?[CodingKeys.remote] ?? "origin"
         branch = dictionary?[CodingKeys.branch] ?? "master"
+        noVerify = dictionary?[CodingKeys.noVerify] ?? false
     }
 }
