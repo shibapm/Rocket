4d8382023224ad794ff8ccbf52cd3d4ecad4be1f
Franco Meloni <franco.meloni91@gmail.com>
2019-01-06 23:22:52 +0000

    Update Logger

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/jpsim/Yams", from: "1.0.0"),
-        .package(url: "https://github.com/f-meloni/Logger", from: "0.2.0"),
+        .package(url: "https://github.com/f-meloni/Logger", from: "0.2.2"),
         .package(url: "https://github.com/JohnSundell/ShellOut.git", from: "2.1.0"),
         .package(url: "https://github.com/orta/PackageConfig.git", from: "0.0.1"),
         .package(url: "https://github.com/Quick/Nimble", from: "7.3.1"),
