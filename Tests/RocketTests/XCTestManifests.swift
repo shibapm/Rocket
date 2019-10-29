addd8394f2ab48e9ea31040063fa1092473cdcb3
Franco Meloni <franco.meloni91@gmail.com>
2019-10-29 22:17:40 +0000

    Generate linux main

--- Tests/RocketTests/XCTestManifests.swift
+++ Tests/RocketTests/XCTestManifests.swift
@@ -1,11 +1,40 @@
 import XCTest
 
+extension BranchExecutorTests {
+    static let __allTests = [
+        ("testItSendsEmptyScriptIfTheDictionaryIsNil", testItSendsEmptyScriptIfTheDictionaryIsNil),
+        ("testItSendsTheCorrectBranchNameToScriptLauncher", testItSendsTheCorrectBranchNameToScriptLauncher),
+        ("testItUsesAWorkaroundToGenerateTheTestsOnLinux", testItUsesAWorkaroundToGenerateTheTestsOnLinux),
+    ]
+}
+
+extension ChecksParserTests {
+    static let __allTests = [
+        ("testItIgnoresTheInvalidSteps", testItIgnoresTheInvalidSteps),
+        ("testItParsesCorrectlyAValidDictionary", testItParsesCorrectlyAValidDictionary),
+    ]
+}
+
+extension CleanGitStatusCheckTests {
+    static let __allTests = [
+        ("testReturnsFalseWhenThereIsAreModifiedFiles", testReturnsFalseWhenThereIsAreModifiedFiles),
+        ("testReturnsTrueWhenThereIsAreNoModifiedFiles", testReturnsTrueWhenThereIsAreNoModifiedFiles),
+    ]
+}
+
 extension CommitExecutorTests {
     static let __allTests = [
         ("testItSetsTheParameterIfTheGitHookFolderExists", testItSetsTheParameterIfTheGitHookFolderExists),
         ("testItUsesAWorkaroundToGenerateTheTestsOnLinux", testItUsesAWorkaroundToGenerateTheTestsOnLinux),
         ("testItUsesTheParametersIfAny", testItUsesTheParametersIfAny),
         ("testItUsesTheStandardCommitMessageIfNoMessageIsProvided", testItUsesTheStandardCommitMessageIfNoMessageIsProvided),
+    ]
+}
+
+extension CurrentVersionProviderTests {
+    static let __allTests = [
+        ("testItReturnsTheCorrectTag", testItReturnsTheCorrectTag),
+        ("testItReturnsTheCorrectVersionIfThereAreNoTags", testItReturnsTheCorrectVersionIfThereAreNoTags),
     ]
 }
 
@@ -37,6 +66,12 @@ extension HideDevDependenciesExecutorTests {
     static let __allTests = [
         ("testItSendsTheHideDependenciesCallToTheDevDependenciesModifier", testItSendsTheHideDependenciesCallToTheDevDependenciesModifier),
         ("testItUsesTheDefaultPackagePath", testItUsesTheDefaultPackagePath),
+    ]
+}
+
+extension NewVersionProviderTests {
+    static let __allTests = [
+        ("testReturnsTheCorrectNewVersion", testReturnsTheCorrectNewVersion),
     ]
 }
 
@@ -74,6 +109,7 @@ extension StepsParserTests {
         ("testItParsesCorrectlyBeforeAndAfterSteps", testItParsesCorrectlyBeforeAndAfterSteps),
         ("testItParsesCorrectlyWhenOnlyAfterIsSpecified", testItParsesCorrectlyWhenOnlyAfterIsSpecified),
         ("testItParsesCorrectlyWhenOnlyBeforeIsSpecified", testItParsesCorrectlyWhenOnlyBeforeIsSpecified),
+        ("testItParsesItReturnsTheDefaultParametersIfTheDictionaryIsEmpty", testItParsesItReturnsTheDefaultParametersIfTheDictionaryIsEmpty),
     ]
 }
 
@@ -102,14 +138,20 @@ extension UnhideDevDependenciesExecutorTests {
 #if !os(macOS)
     public func __allTests() -> [XCTestCaseEntry] {
         return [
+            testCase(BranchExecutorTests.__allTests),
+            testCase(ChecksParserTests.__allTests),
+            testCase(CleanGitStatusCheckTests.__allTests),
             testCase(CommitExecutorTests.__allTests),
+            testCase(CurrentVersionProviderTests.__allTests),
             testCase(DefaultExecutorStepDescriptionTests.__allTests),
             testCase(DevDependenciesModifierTests.__allTests),
             testCase(GitAddExecutorTests.__allTests),
             testCase(HideDevDependenciesExecutorTests.__allTests),
+            testCase(NewVersionProviderTests.__allTests),
             testCase(PushExecutorTests.__allTests),
             testCase(RocketFileFinderTests.__allTests),
             testCase(ScriptExecutorTests.__allTests),
+            testCase(ScriptLauncherTestCase.__allTests),
             testCase(StepsParserTests.__allTests),
             testCase(SwiftScriptExecutorTests.__allTests),
             testCase(TagExecutorTests.__allTests),
