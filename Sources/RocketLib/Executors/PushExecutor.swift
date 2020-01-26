f0182c5514e34ee074297af9bcda3f23f5734c8a
Franco Meloni <franco.meloni91@gmail.com>
2020-01-26 23:33:26 +0000

    Improve NoVerifyParameterInserter

--- Sources/RocketLib/Executors/PushExecutor.swift
+++ Sources/RocketLib/Executors/PushExecutor.swift
@@ -3,9 +3,8 @@ import Logger
 
 final class PushExecutor: NoVerifyParameterInserterExecutor<PushParameters> {
     override func executeStep(version: String, logger: Logger) {
-        var scriptString = "git push \(parameters.remote) \(parameters.branch) --tags"
-
-        parameterInserter.insertNoVerifyParameterIfNeeded(string: &scriptString, noVerifyParameter: parameters.noVerify)
+        let scriptString = parameterInserter.insertNoVerifyParameterIfNeeded(string: "git push \(parameters.remote) \(parameters.branch) --tags",
+                                                                             noVerifyParameter: parameters.noVerify)
 
         launchScript(content: scriptString, version: version, errorMessage: "Push step failed with error", logger: logger)
     }
